1561c50809f3150f535cc08e751b7ec4c1bc43e4
ManuelCrispino <manuel.barman96@gmail.com>
2025-04-12 03:25:41 +0200

    finish-app

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,41 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Player() {
-  const [input,setInput]= useState({
-    name: "",
-    isSubmitted:false
-  });
+  const playerName = useRef();
+  const [name,setName]= useState(null);
 
- function  handleInput(e){
 
-  const newName = e.target.value;
-  setInput(prev => ({
-    ...prev,
-    name: newName,
-  }));
-  
-  if( newName === ""){
-    setInput(prev => ({
-      ...prev,
-      isSubmitted:false}));
-    };
-
-  }; 
- 
   function handleClick(){
-    if ( input.name !== ""){
-   setInput(prev => ({
-    ...prev,
-    isSubmitted:true}));}
+    const newName = playerName.current.value; 
+   const capitalize = newName.charAt(0).toUpperCase() + newName.slice(1).toLowerCase()
+    setName(capitalize);
+    playerName.current.value = "";
   };
 
   return (
     <section id="player">
-      <h2>Welcome  {input.isSubmitted ? input.name : "unknown entity" }</h2>
+      <h2>Welcome  {name || "unknown entity"}</h2> 
+      {/* || funziona come ternario ? : in questo caso meglio di ?? */}
       <p>
-        <input type="text" onChange={handleInput} value={input.name} />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button >
       </p>
     </section>
   );
-}
+};
---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,28 @@
+import React, { useImperativeHandle, useRef } from "react";
+import {createPortal} from 'react-dom';
+
+
+export default function ResultModal ({refDialog,targetTime,remainingTime,handleReset}){
+    const dialog = useRef();
+    const userLost = remainingTime <= 0 ; 
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000))* 100);
+    useImperativeHandle(refDialog,()=>{
+      return{  open(){
+        dialog.current.showModal();
+    }};
+    })
+
+return createPortal(
+    <dialog ref={dialog} onClose={handleReset()} className="result-modal" >
+     {userLost && <h2>You Lost</h2>} 
+     {!userLost && <h2>Your Score : {score}</h2>}  
+    <p>the target time was <strong>{targetTime}</strong></p>
+    <p>you stop the timer with <strong>{formattedRemainingTime} seconds left</strong> </p>
+    <form method="dialog" onSubmit={handleReset()}>
+        <button>close</button>
+    </form>
+    </dialog>,
+    document.getElementById('modal')
+);
+}
